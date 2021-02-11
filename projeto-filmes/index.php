8bf65d6dcd8f3cc0eb778a6a5e35cf77d6972071
luisa <luisa@DESKTOP-O1O6AUM.local>
2021-02-11 12:22:05 +0000

    sistema login - parte 2

--- projeto-filmes/index.php
+++ projeto-filmes/index.php
@@ -1,7 +1,8 @@
-<?php   
-    $con=new mysqli("localhost","root","","filmes");
+<?php
+    session_start();
+    $con = new mysqli("localhost","root","","filmes");
     if($con->connect_errno!=0){
-        echo "Ocorreu um erro no acesso à base de dados ". $con->connect_error;
+        echo "Ocorreu um erro no acesso à base de dados ".$con->connect_error;
         exit;
     }
     else{
@@ -9,31 +10,43 @@
     <!DOCTYPE html>
     <html>
     <head>
-        <meta charset="ISO-8859-1">
-        <title>filmes</title>
+    <meta charset="ISO-8859-1">
+    <title>Filmes</title>
     </head>
     <body>
-        <h1>Lista de filmes</h1>
-            <?php   
-                $stm= $con->prepare('select * from filmes');
-                $stm->execute();
-                $res=$stm->get_result();
-                while($resultado = $res->fetch_assoc()){
-                    echo '<a href="filmes_show.php?filme='.$resultado['id_filme']. '">';
-                    echo $resultado['titulo'];
-                    echo '</a>';
-                    echo '<br>';
-                }
-                $stm->close();
-            ?>
-            <br> <br>
-            <a href="filmes_create.php">Adicionar</a>
-            <a href="login.php">Login</a>
-            <a href="register.php">Register</a>
-            <a href="listautilizadores.php">ListadeUtilizadores</a>
+    <h1>filmes</h1>
+    <?php
+        $stm = $con->prepare('select * from filmes');
+        $stm->execute();
+        $res=$stm->get_result();
+        while($resultado = $res->fetch_assoc()){
+            echo '<a href="filmes_show.php?filme='.$resultado['id_filme'].'">';
+            echo $resultado['titulo'];
+            echo '</a>';
+            echo '<br>';
+        }
+        $stm->close();
+        echo "<br>";
+        $stm = $con->prepare('select * from utilizadores');
+        $stm->execute();
+        $res=$stm->get_result();
+        while($resultado = $res->fetch_assoc()){
+            if($resultado['id'] == $_SESSION['id_user']){
+                echo '<a href="utilizadores_edit.php?user='.$resultado['id'].'">User</a><br>';
+            }
+        }
+        $stm->close();
+        ?>
+         <br> <br>
+        <a href="filmes_create.php">Adicionar Filme</a>
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+        <a href="listautilizadores.php">Lista de utilizadores</a>
+    
         <br>
     </body>
-    </html>
+</html>
+
 <?php
     }
 ?>
